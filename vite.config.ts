6257c045bd6eb8ce77110a8ae3b68139a80f5043
Hylton-9 <jaydonhylton17@gmail.com>
2025-08-30 22:16:51 -0500

    Update vite.config.ts configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    base: '/BudgetApp/'
     return {
       define: {
+        
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
